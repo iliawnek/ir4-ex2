4ec8c54f8755de924803b2570a2c90dccdf23976
Ken Li <iliawnek@gmail.com>
2018-03-03 18:18:23 +0000

    Simplify AveragePositionDifferenceDSM

--- src/main/java/uk/ac/gla/dcs/dsms/AveragePositionDifferenceDSM.java
+++ src/main/java/uk/ac/gla/dcs/dsms/AveragePositionDifferenceDSM.java
@@ -5,42 +5,44 @@ import org.terrier.structures.postings.BlockPosting;
 import org.terrier.matching.dsms.DependenceScoreModifier;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-/** 
+/**
  * You should use this sample class to implement a proximity feature in Exercise 2.
  * TODO: Describe the function that your class implements
  * <p>
  * You can add your feature into a learned model by appending DSM:uk.ac.gla.IRcourse.AveragePositionDifferenceDSM to the features.list file.
+ *
  * @author TODO
  */
 public class AveragePositionDifferenceDSM extends DependenceScoreModifier {
 
 
-	/** This class is passed the postings of the current document,
-	 * and should return a score to represent that document.
-	 */
-	@Override
-	protected double calculateDependence(
-			IterablePosting[] ips, //posting lists
-			boolean[] okToUse,  //is this posting list on the correct document?
-			double[] phraseTermWeights, boolean SD //not needed
-		)
-	{
-		// get all posting lists for this document
-        List<IterablePosting> relevantPostingLists = new ArrayList<>();
-		for (int i = 0; i < okToUse.length; i++) {
-		    if (okToUse[i]) relevantPostingLists.add(ips[i]);
+    /**
+     * This class is passed the postings of the current document,
+     * and should return a score to represent that document.
+     */
+    @Override
+    protected double calculateDependence(
+            IterablePosting[] ips, //posting lists
+            boolean[] okToUse,  //is this posting list on the correct document?
+            double[] phraseTermWeights, boolean SD //not needed
+    ) {
+        // get all position vectors for this document
+        List<int[]> positionVectors = new ArrayList<>();
+        for (int i = 0; i < okToUse.length; i++) {
+            if (okToUse[i]) {
+                int[] positionVector = ((BlockPosting) ips[i]).getPositions();
+                positionVectors.add(positionVector);
+            }
         }
 
         // return if not enough terms to calculate proximity
-        if (relevantPostingLists.size() < 2) return 0.0d;
+        if (positionVectors.size() < 2) return 0.0d;
 
         // compute average position for each query term
         List<Double> averagePositions = new ArrayList<>();
-		for (IterablePosting ip : relevantPostingLists) {
-            int[] positionVector = ((BlockPosting) ip).getPositions();
+        for (int[] positionVector : positionVectors) {
             double sum = 0.0d;
             for (int pos : positionVector) sum += pos;
             double avg = sum / positionVector.length;
@@ -50,8 +52,8 @@ public class AveragePositionDifferenceDSM extends DependenceScoreModifier {
         // compute difference between average positions for each pair
         List<Double> differences = new ArrayList<>();
         for (int i = 0; i < averagePositions.size(); i++) {
-		    for (int j = i + 1; j < averagePositions.size(); j++) {
-		        double difference = Math.abs(averagePositions.get(i) - averagePositions.get(j));
+            for (int j = i + 1; j < averagePositions.size(); j++) {
+                double difference = Math.abs(averagePositions.get(i) - averagePositions.get(j));
                 differences.add(difference);
             }
         }
@@ -61,18 +63,20 @@ public class AveragePositionDifferenceDSM extends DependenceScoreModifier {
         for (double diff : differences) sum += diff;
         double avg = sum / differences.size();
         return avg;
-	}
+    }
 
-	/** You do NOT need to implement this method */
-	@Override
-	protected double scoreFDSD(int matchingNGrams, int docLength) {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * You do NOT need to implement this method
+     */
+    @Override
+    protected double scoreFDSD(int matchingNGrams, int docLength) {
+        throw new UnsupportedOperationException();
+    }
 
 
-	@Override
-	public String getName() {
-		return "ProxFeatureDSM_AveragePositionDifference";
-	}
-	
+    @Override
+    public String getName() {
+        return "ProxFeatureDSM_AveragePositionDifference";
+    }
+
 }
