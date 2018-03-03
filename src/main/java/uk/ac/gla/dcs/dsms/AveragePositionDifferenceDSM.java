b09a7cdae659da1f91a7ea4e703382cdd8efa90f
Ken Li <iliawnek@gmail.com>
2018-03-03 17:00:35 +0000

    AveragePositionDifferenceDSM

--- src/main/java/uk/ac/gla/dcs/dsms/AveragePositionDifferenceDSM.java
+++ src/main/java/uk/ac/gla/dcs/dsms/AveragePositionDifferenceDSM.java
@@ -1,7 +1,12 @@
 package uk.ac.gla.dcs.dsms;
 
 import org.terrier.structures.postings.IterablePosting;
+import org.terrier.structures.postings.BlockPosting;
 import org.terrier.matching.dsms.DependenceScoreModifier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /** 
  * You should use this sample class to implement a proximity feature in Exercise 2.
@@ -21,19 +26,41 @@ public class AveragePositionDifferenceDSM extends DependenceScoreModifier {
 			IterablePosting[] ips, //posting lists
 			boolean[] okToUse,  //is this posting list on the correct document?
 			double[] phraseTermWeights, boolean SD //not needed
-		) 
+		)
 	{
-		
-		final int numberOfQueryTerms = okToUse.length;
+		// get all posting lists for this document
+        List<IterablePosting> relevantPostingLists = new ArrayList<>();
+		for (int i = 0; i < okToUse.length; i++) {
+		    if (okToUse[i]) relevantPostingLists.add(ips[i]);
+        }
 
-		//*** 
-		//TODO: in this part, write your code that inspects
-		//the positions of query terms, to make a proximity function
-		//NB: you can cast each IterablePosting to org.terrier.structures.postings.BlockPosting
-		//and use the getPositions() method to obtain the positions.
-		//***
-		
-		return 0.0d;
+        // return if not enough terms to calculate proximity
+        if (relevantPostingLists.size() < 2) return 0.0d;
+
+        // compute average position for each query term
+        List<Double> averagePositions = new ArrayList<>();
+		for (IterablePosting ip : relevantPostingLists) {
+            int[] positionVector = ((BlockPosting) ip).getPositions();
+            double sum = 0.0d;
+            for (int pos : positionVector) sum += pos;
+            double avg = sum / positionVector.length;
+            averagePositions.add(avg);
+        }
+
+        // compute difference between average positions for each pair
+        List<Double> differences = new ArrayList<>();
+        for (int i = 0; i < averagePositions.size(); i++) {
+		    for (int j = i + 1; j < averagePositions.size(); j++) {
+		        double difference = Math.abs(averagePositions.get(i) - averagePositions.get(j));
+                differences.add(difference);
+            }
+        }
+
+        // compute average across all differences (i.e. aggregate for entire query)
+        double sum = 0.0d;
+        for (double diff : differences) sum += diff;
+        double avg = sum / differences.size();
+        return avg;
 	}
 
 	/** You do NOT need to implement this method */
@@ -45,7 +72,7 @@ public class AveragePositionDifferenceDSM extends DependenceScoreModifier {
 
 	@Override
 	public String getName() {
-		return "ProxFeatureDSM_MYFUNCTION";
+		return "ProxFeatureDSM_AveragePositionDifference";
 	}
 	
 }
--- src/test/java/uk/ac/gla/dcs/dsms/TestAveragePositionDifferenceDSM.java
+++ src/test/java/uk/ac/gla/dcs/dsms/TestAveragePositionDifferenceDSM.java
@@ -12,35 +12,148 @@ import org.terrier.structures.postings.IterablePosting;
 import org.terrier.tests.ApplicationSetupBasedTest;
 import org.terrier.utility.ApplicationSetup;
 
-public class TestAveragePositionDifferenceDSM extends ApplicationSetupBasedTest
-{
-	@Test public void testOneDocTwoTerms() throws Exception {
+public class TestAveragePositionDifferenceDSM extends ApplicationSetupBasedTest {
+    @Test
+    public void testTwoTermsOneOccurrence() throws Exception {
 
-		//make an index with a single sample document
-		ApplicationSetup.setProperty("termpipelines", "");
-		Index index = IndexTestUtils.makeIndexBlocks(
-				new String[]{"doc1"}, 
-				new String[]{"The quick brown fox jumps over the lazy dog"});
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick brown fox jumps over the lazy dog."});
 
-		//get posting iterators for two terms 'fox' and 'jumps'
-		IterablePosting[] ips = new IterablePosting[2];
-		ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
-		ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("jumps"));
-		ips[0].next();
-		ips[1].next();
-		assertEquals(0, ips[0].getId());
-		assertEquals(0, ips[1].getId());
-		System.out.println("Positions of term 'fox'="+ Arrays.toString( ((BlockPosting)ips[0]).getPositions()));
-		System.out.println("Positions of term 'jumps'="+ Arrays.toString( ((BlockPosting)ips[1]).getPositions()));
+        // get posting iterators for two terms 'fox' and 'jumps'
+        IterablePosting[] ips = new IterablePosting[2];
+        System.out.println("LEXICONENTRY = " + index.getLexicon().getLexiconEntry("fox"));
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("jumps"));
+        ips[0].next();
+        ips[1].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'jumps' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
 
-		AveragePositionDifferenceDSM sample = new AveragePositionDifferenceDSM();
-		double score = sample.calculateDependence(
-            ips, //posting lists
-            new boolean[]{true,true},  //is this posting list on the correct document?
-            new double[]{1d,1d}, false//doesnt matter
-		);
-		System.out.println(score);
-		//TODO: make your assertion about what the score should be
-		//assertEquals(XXX, score, 0.0d);
-	}
+        // compute score
+        AveragePositionDifferenceDSM sample = new AveragePositionDifferenceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {true, true}, // is this posting list on the correct document?
+                new double[] {1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        assertEquals(1.0d, score, 0.0d);
+    }
+
+    @Test
+    public void testTwoTermsManyOccurrences() throws Exception {
+
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick fox and brown fox both jump over the quick dog."});
+
+        // get posting iterators for two terms 'quick' and 'fox'
+        IterablePosting[] ips = new IterablePosting[2];
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("quick"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[0].next();
+        ips[1].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        System.out.println("Positions of term 'quick' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
+
+        // compute score
+        AveragePositionDifferenceDSM sample = new AveragePositionDifferenceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {true, true}, // is this posting list on the correct document?
+                new double[] {1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        final double averagePositionForQuick = (1 + 10) / 2;
+        final double averagePositionForFox = (2 + 5) / 2;
+        final double difference = Math.abs(averagePositionForFox - averagePositionForQuick);
+        assertEquals(difference, score, 0.0d);
+    }
+
+    @Test
+    public void testManyTermsManyOccurrences() throws Exception {
+
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick fox and brown dog and fox dog all jump over the quick fox and brown fox and quick fox."});
+
+        // get posting iterators for three terms 'quick', 'fox', and 'brown'
+        IterablePosting[] ips = new IterablePosting[3];
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("quick"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[2] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("brown"));
+        ips[0].next();
+        ips[1].next();
+        ips[2].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        assertEquals(0, ips[2].getId());
+        System.out.println("Positions of term 'quick' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
+        System.out.println("Positions of term 'brown' = " + Arrays.toString(((BlockPosting) ips[2]).getPositions()));
+
+        // compute score
+        AveragePositionDifferenceDSM sample = new AveragePositionDifferenceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {true, true, true}, // is this posting list on the correct document?
+                new double[] {1d, 1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        final double averagePositionForQuick = (1 + 13 + 19) / 3;
+        final double averagePositionForFox = (2 + 7 + 14 + 17 + 20) / 5;
+        final double averagePositionForBrown = (4 + 16) / 2;
+        final double differenceBetweenQuickAndFox = Math.abs(averagePositionForFox - averagePositionForQuick);
+        final double differenceBetweenQuickAndBrown = Math.abs(averagePositionForBrown - averagePositionForQuick);
+        final double differenceBetweenFoxAndBrown = Math.abs(averagePositionForBrown - averagePositionForFox);
+        final double averageDifference = (
+                differenceBetweenQuickAndFox +
+                differenceBetweenQuickAndBrown +
+                differenceBetweenFoxAndBrown
+                ) / 3;
+        assertEquals(averageDifference, score, 0.0d);
+    }
+
+    @Test
+    public void testNoQueryTerms() throws Exception {
+
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick brown fox jumps over the lazy dog."});
+
+        // get posting iterators for two terms 'fox' and 'jumps'
+        IterablePosting[] ips = new IterablePosting[2];
+        System.out.println("LEXICONENTRY = " + index.getLexicon().getLexiconEntry("fox"));
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("jumps"));
+        ips[0].next();
+        ips[1].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'jumps' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
+
+        // compute score
+        AveragePositionDifferenceDSM sample = new AveragePositionDifferenceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {false, false}, // is this posting list on the correct document?
+                new double[] {1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        assertEquals(0.0d, score, 0.0d);
+    }
 }
