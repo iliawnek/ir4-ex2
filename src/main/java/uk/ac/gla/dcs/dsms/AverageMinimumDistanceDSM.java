a14d0039e10b5647a01da67f9679177b7efaafbe
Ken Li <iliawnek@gmail.com>
2018-03-03 18:17:52 +0000

    AverageMinimumDistanceDSM

--- src/main/java/uk/ac/gla/dcs/dsms/AverageMinimumDistanceDSM.java
+++ src/main/java/uk/ac/gla/dcs/dsms/AverageMinimumDistanceDSM.java
@@ -1,51 +1,89 @@
 package uk.ac.gla.dcs.dsms;
 
-import org.terrier.matching.dsms.DependenceScoreModifier;
 import org.terrier.structures.postings.IterablePosting;
+import org.terrier.structures.postings.BlockPosting;
+import org.terrier.matching.dsms.DependenceScoreModifier;
 
-/** 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
  * You should use this sample class to implement a proximity feature in Exercise 2.
  * TODO: Describe the function that your class implements
  * <p>
  * You can add your feature into a learned model by appending DSM:uk.ac.gla.IRcourse.AveragePositionDifferenceDSM to the features.list file.
+ *
  * @author TODO
  */
 public class AverageMinimumDistanceDSM extends DependenceScoreModifier {
 
+    /**
+     * This class is passed the postings of the current document,
+     * and should return a score to represent that document.
+     */
+    @Override
+    protected double calculateDependence(
+            IterablePosting[] ips, // posting lists
+            boolean[] okToUse,  // is this posting list on the correct document?
+            double[] phraseTermWeights, boolean SD // not needed
+    ) {
 
-	/** This class is passed the postings of the current document,
-	 * and should return a score to represent that document.
-	 */
-	@Override
-	protected double calculateDependence(
-			IterablePosting[] ips, //posting lists
-			boolean[] okToUse,  //is this posting list on the correct document?
-			double[] phraseTermWeights, boolean SD //not needed
-		) 
-	{
-		
-		final int numberOfQueryTerms = okToUse.length;
+        // get all position vectors for this document
+        List<int[]> positionVectors = new ArrayList<>();
+        for (int i = 0; i < okToUse.length; i++) {
+            if (okToUse[i]) {
+                int[] positionVector = ((BlockPosting) ips[i]).getPositions();
+                positionVectors.add(positionVector);
+            }
+        }
 
-		//*** 
-		//TODO: in this part, write your code that inspects
-		//the positions of query terms, to make a proximity function
-		//NB: you can cast each IterablePosting to org.terrier.structures.postings.BlockPosting
-		//and use the getPositions() method to obtain the positions.
-		//***
-		
-		return 0.0d;
-	}
+        // return if not enough terms to calculate proximity
+        if (positionVectors.size() < 2) return 0.0d;
 
-	/** You do NOT need to implement this method */
-	@Override
-	protected double scoreFDSD(int matchingNGrams, int docLength) {
-		throw new UnsupportedOperationException();
-	}
+        // compute average minimum distance between each pair
+        List<Double> avgMinDistances = new ArrayList<>();
+        for (int i = 0; i < positionVectors.size(); i++) {
+            for (int j = i + 1; j < positionVectors.size(); j++) {
+                // identify which position vector has fewer/more occurrences
+                int[] pvi = positionVectors.get(i);
+                int[] pvj = positionVectors.get(j);
+                int[] pvFewer = pvi.length <= pvj.length ? pvi : pvj;
+                int[] pvMore = pvi.length > pvj.length ? pvi : pvj;
+
+                // compute average of minimum distances between each occurrence in pvFewer and any occurrence in pvMore
+                int sum = 0;
+                for (int posFewer : pvFewer) {
+                    int min = Integer.MAX_VALUE;
+                    for (int posMore : pvMore) {
+                        int dist = Math.abs(posFewer - posMore);
+                        if (dist < min) min = dist;
+                    }
+                    sum += min;
+                }
+                double avg = sum / pvFewer.length;
+                avgMinDistances.add(avg);
+            }
+        }
+
+        // compute average of average minimum distances across all pairs
+        double sum = 0.0d;
+        for (double d : avgMinDistances) sum += d;
+        double avg = sum / avgMinDistances.size();
+        return avg;
+    }
+
+    /**
+     * You do NOT need to implement this method
+     */
+    @Override
+    protected double scoreFDSD(int matchingNGrams, int docLength) {
+        throw new UnsupportedOperationException();
+    }
 
 
-	@Override
-	public String getName() {
-		return "ProxFeatureDSM_MYFUNCTION";
-	}
-	
+    @Override
+    public String getName() {
+        return "ProxFeatureDSM_AveragePositionDifference";
+    }
+
 }
--- src/test/java/uk/ac/gla/dcs/dsms/TestAverageMinimumDistanceDSM.java
+++ src/test/java/uk/ac/gla/dcs/dsms/TestAverageMinimumDistanceDSM.java
@@ -12,35 +12,174 @@ import org.terrier.structures.postings.IterablePosting;
 import org.terrier.tests.ApplicationSetupBasedTest;
 import org.terrier.utility.ApplicationSetup;
 
-public class TestAverageMinimumDistanceDSM extends ApplicationSetupBasedTest
-{
-	@Test public void testOneDocTwoTerms() throws Exception {
+public class TestAverageMinimumDistanceDSM extends ApplicationSetupBasedTest {
+    @Test
+    public void testTwoTermsOneOccurrence() throws Exception {
 
-		//make an index with a single sample document
-		ApplicationSetup.setProperty("termpipelines", "");
-		Index index = IndexTestUtils.makeIndexBlocks(
-				new String[]{"doc1"}, 
-				new String[]{"The quick brown fox jumps over the lazy dog"});
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick brown fox jumps over the lazy dog."});
 
-		//get posting iterators for two terms 'fox' and 'jumps'
-		IterablePosting[] ips = new IterablePosting[2];
-		ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
-		ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("jumps"));
-		ips[0].next();
-		ips[1].next();
-		assertEquals(0, ips[0].getId());
-		assertEquals(0, ips[1].getId());
-		System.out.println("Positions of term 'fox'="+ Arrays.toString( ((BlockPosting)ips[0]).getPositions()));
-		System.out.println("Positions of term 'jumps'="+ Arrays.toString( ((BlockPosting)ips[1]).getPositions()));
+        // get posting iterators for two terms 'fox' and 'jumps'
+        IterablePosting[] ips = new IterablePosting[2];
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("jumps"));
+        ips[0].next();
+        ips[1].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'jumps' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
 
-		AverageMinimumDistanceDSM sample = new AverageMinimumDistanceDSM();
-		double score = sample.calculateDependence(
-            ips, //posting lists
-            new boolean[]{true,true},  //is this posting list on the correct document?
-            new double[]{1d,1d}, false//doesnt matter
-		);
-		System.out.println(score);
-		//TODO: make your assertion about what the score should be
-		//assertEquals(XXX, score, 0.0d);
-	}
+        // compute score
+        AverageMinimumDistanceDSM sample = new AverageMinimumDistanceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {true, true}, // is this posting list on the correct document?
+                new double[] {1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        assertEquals(1.0d, score, 0.0d);
+    }
+
+    @Test
+    public void testTwoTermsSameNumberOfOccurrences() throws Exception {
+
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick fox and brown fox both jump over the quick dog."});
+
+        // get posting iterators for two terms 'quick' and 'fox'
+        IterablePosting[] ips = new IterablePosting[2];
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("quick"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[0].next();
+        ips[1].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        System.out.println("Positions of term 'quick' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
+
+        // compute score
+        AverageMinimumDistanceDSM sample = new AverageMinimumDistanceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {true, true}, // is this posting list on the correct document?
+                new double[] {1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        final double avgMinDist = (1 + 5) / 2;
+        assertEquals(avgMinDist, score, 0.0d);
+    }
+
+    @Test
+    public void testTwoTermsDifferentNumberOfOccurrences() throws Exception {
+
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick fox and brown fox both jump over the quick fox."});
+
+        // get posting iterators for two terms 'quick' and 'fox'
+        IterablePosting[] ips = new IterablePosting[2];
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("quick"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[0].next();
+        ips[1].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        System.out.println("Positions of term 'quick' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
+
+        // compute score
+        AverageMinimumDistanceDSM sample = new AverageMinimumDistanceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {true, true}, // is this posting list on the correct document?
+                new double[] {1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        final double avgMinDist = (1 + 1) / 2;
+        assertEquals(avgMinDist, score, 0.0d);
+    }
+
+    @Test
+    public void testManyTermsManyOccurrences() throws Exception {
+
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick fox and brown dog and fox dog all jump over the quick fox and brown fox and quick fox."});
+
+        // get posting iterators for three terms 'quick', 'fox', and 'brown'
+        IterablePosting[] ips = new IterablePosting[3];
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("quick"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[2] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("brown"));
+        ips[0].next();
+        ips[1].next();
+        ips[2].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        assertEquals(0, ips[2].getId());
+        System.out.println("Positions of term 'quick' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
+        System.out.println("Positions of term 'brown' = " + Arrays.toString(((BlockPosting) ips[2]).getPositions()));
+
+        // compute score
+        AverageMinimumDistanceDSM sample = new AverageMinimumDistanceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {true, true, true}, // is this posting list on the correct document?
+                new double[] {1d, 1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        final double avgMinDistQuickFox = (1 + 1 + 1) / 3;
+        final double avgMinDistQuickBrown = (3 + 3) / 2;
+        final double avgMinDistFoxBrown = (2 + 1) / 2;
+        final double avgMinDist = (
+                avgMinDistQuickFox +
+                avgMinDistQuickBrown +
+                avgMinDistFoxBrown
+                ) / 3;
+        assertEquals(avgMinDist, score, 0.0d);
+    }
+
+    @Test
+    public void testNoQueryTerms() throws Exception {
+
+        // make an index with a single sample document
+        ApplicationSetup.setProperty("termpipelines", "");
+        Index index = IndexTestUtils.makeIndexBlocks(
+                new String[]{"doc1"},
+                new String[]{"The quick brown fox jumps over the lazy dog."});
+
+        // get posting iterators for two terms 'fox' and 'jumps'
+        IterablePosting[] ips = new IterablePosting[2];
+        System.out.println("LEXICONENTRY = " + index.getLexicon().getLexiconEntry("fox"));
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("jumps"));
+        ips[0].next();
+        ips[1].next();
+        assertEquals(0, ips[0].getId());
+        assertEquals(0, ips[1].getId());
+        System.out.println("Positions of term 'fox' = " + Arrays.toString(((BlockPosting) ips[0]).getPositions()));
+        System.out.println("Positions of term 'jumps' = " + Arrays.toString(((BlockPosting) ips[1]).getPositions()));
+
+        // compute score
+        AverageMinimumDistanceDSM sample = new AverageMinimumDistanceDSM();
+        double score = sample.calculateDependence(
+                ips, // posting lists
+                new boolean[] {false, false}, // is this posting list on the correct document?
+                new double[] {1d, 1d}, false // doesn't matter
+        );
+        System.out.println("Score = " + score);
+        assertEquals(0.0d, score, 0.0d);
+    }
 }
